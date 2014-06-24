b56e93a6d35c7b00cccc9b14a65b093ebbde5be5
7sharp9 <kukulcanenator@googlemail.com>
2014-06-24 10:50:49 +0100

    Initial checkin of all files

---
+++ XamarinFormsTest.iOS/AppDelegate.fs
@@ -0,0 +1,19 @@
+﻿namespace XamarinFormsTest
+
+open System
+open MonoTouch.UIKit
+open MonoTouch.Foundation
+open Xamarin.Forms
+
+[<Register ("AppDelegate")>]
+type AppDelegate() =
+    inherit UIApplicationDelegate()
+
+    let mutable window = Unchecked.defaultof<UIWindow>
+
+    override x.FinishedLaunching (app, options) =
+        Forms.Init ()
+        window <- new UIWindow (UIScreen.MainScreen.Bounds)
+        window.RootViewController = App.GetMainPage().CreateViewController () |> ignore
+        window.MakeKeyAndVisible ()
+        true        
---
+++ XamarinFormsTest/App.fs
@@ -0,0 +1,13 @@
+﻿namespace XamarinFormsTest
+
+open Xamarin.Forms
+
+    type App =
+
+        static member GetMainPage () =
+ 
+            let cp = ContentPage() 
+            cp.Content <- Label(Text = "Hello, Forms!",
+                                VerticalOptions = LayoutOptions.CenterAndExpand,
+                                HorizontalOptions = LayoutOptions.CenterAndExpand)
+            cp :> Page
